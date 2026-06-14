3aff5ab10a2804f2e96f14b4d1df881ef320908d
Tezcan Ergene <tezcane@users.noreply.github.com>
2026-06-14 14:46:24 +0300

    Fix hashCode implementation in AssetFlare

--- flare_flutter/lib/provider/asset_flare.dart
+++ flare_flutter/lib/provider/asset_flare.dart
@@ -21,7 +21,7 @@ class AssetFlare extends AssetProvider {
   });
 
   @override
-  int get hashCode => int get hashCode => Object.hash(bundle, name);
+  int get hashCode => Object.hash(bundle, name);
 
   @override
   bool operator ==(dynamic other) {
